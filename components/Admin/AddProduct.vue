3d26d922f4b4eb8d204adeb1219a5f153aeb7d4a
matilda.hoglund <hoglund.matilda@gmail.com>
2021-05-13 20:00:54 +0200

    add product css

--- components/Admin/AddProduct.vue
+++ components/Admin/AddProduct.vue
@@ -2,15 +2,23 @@
   <div class="add-product__wrapper">
     <h2>Lägg till ny produkt</h2>
     <section class="add-product__section-form">
-      <input type="text" placeholder="Namn" />
+      <input v-model="product.name" type="text" placeholder="Namn" />
       <div class="input-row">
-        <input type="text" placeholder="Kategori" />
-        <input type="text" placeholder="Storlek" />
-        <input type="text" placeholder="Pris" />
+        <input v-model="product.category" type="text" placeholder="Kategori" />
+        <input
+          v-model="product.size"
+          id="strl"
+          type="text"
+          placeholder="Storlek"
+        />
+        <input v-model="product.price" type="text" placeholder="Pris" />
       </div>
-      <textarea placeholder="Beskrivning"></textarea>
+      <textarea
+        v-model="product.description"
+        placeholder="Beskrivning"
+      ></textarea>
+      <button @click="addProduct">Lägg till</button>
     </section>
-    <button @click="addProduct">ADD PRODUCT</button>
   </div>
 </template>
 
@@ -18,13 +26,62 @@
 import { mapActions } from 'vuex'
 
 export default {
+  data() {
+    return {
+      product: {
+        name: '',
+        category: '',
+        size: '',
+        description: '',
+        price: '',
+      },
+    }
+  },
   methods: {
     ...mapActions(['postProduct']),
     addProduct() {
-      this.postProduct()
+      const isEmpty = !Object.values(this.product).some((key) => key !== '')
+      if (isEmpty === false) this.postProduct(this.product)
+      if (isEmpty === true) this.postProduct()
     },
   },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+input {
+  @include input;
+}
+
+textarea {
+  @include input;
+  border-radius: 20px;
+  height: 10rem;
+}
+
+button {
+  @include btn;
+}
+
+.add-product__wrapper {
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  margin: 3rem;
+
+  .add-product__section-form {
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+
+    .input-row {
+      display: flex;
+      justify-content: space-between;
+
+      #strl {
+        margin: 16px 6px;
+      }
+    }
+  }
+}
+</style>
--- store/actions.js
+++ store/actions.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default {
   async getProducts({ commit }, id) {
     const products = await axios
-      .get(`http://localhost:5000/api/Products`, {})
+      .get(`/api/Products`, {})
       .then((response) => response.data)
       .catch((error) => {
         console.log(error)
@@ -12,12 +12,23 @@ export default {
     commit('GET_ALL_PRODUCTS', products)
   },
 
+  async postProduct({ commit }, product) {
+    let data
+    if (product) {
+      data = product
+    }
 
-  async postProduct({ commit },) {
+    if (!product) {
+      data = {
+        name: 'test chilli',
+        quantity: 3,
+        cost: 135,
+        category: 'Salsa',
+      }
+    }
 
-    const data = {name: "test chilli", quantity: 3, cost: 135, category: "Salsa"}
     const products = await axios
-      .post(`/api/Products`, {data})
+      .post(`/api/Products`, { data })
       .then((response) => response.data)
       .catch((error) => {
         console.log(error)
